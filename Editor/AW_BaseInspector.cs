63c482efd4d80069f417157e540148834e920df6
uhKayla <hey@kayla.lol>
2024-01-12 19:13:23 -0500

    Remove broken penetrator

--- Editor/AW_BaseInspector.cs
+++ Editor/AW_BaseInspector.cs
@@ -18,8 +18,7 @@ namespace ANGELWARE.AW_APS.Editor
 
         protected virtual void SetupContent(VisualElement root)
         {
-            // Add default or custom content here
-            // Example: root.Add(new Label("Override SetupContent to customize"));
+            
         }
     }
 }
